80672e562c86f8f9d3edfcab88c4979f33ebe126
ssujahudeen <sujahudeen.sahir@gmail.com>
2023-04-02 16:42:30 +0200

    fixed untracked files

--- utilities/panair/locfcn.f90
+++
@@ -1,9 +0,0 @@
-      integer function locfcn(ia)
-! ----------------------------------------------------------------------
-      IMPLICIT NONE
-      INTEGER ia(*)
-!      locfcn  =  POINTER( ia(1) )
-  locfcn = LOC(ia(1))
-      return
-                            ! ==========================================
-      END Function Locfcn
